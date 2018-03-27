293d14a03b355777801576bf0f779182a64819d1
Dlaptiev <37386641+Dlaptiev@users.noreply.github.com>
2018-03-27 17:59:14 -0400

    First commit

---
+++ function.js
@@ -0,0 +1,15 @@
+var a= 3
+
+function getUser(id,name,email){
+    var id=id
+    var name=name
+    var email=email
+
+    console.log("This users id id ${id}...")
+}
+function sendBack(a){
+    return x*3
+}
+getUser("AB1234","Jeremy","janderson@techlaunch")
+
+var b= sendBack(a)
